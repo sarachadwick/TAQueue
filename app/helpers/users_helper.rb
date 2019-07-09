16e5d91b16dcbefb46d823b51f26522b8df90df2
Kristi Bresciano <kbresciano@instructure.com>
2019-07-09 17:18:20 -0600

    show only present TAs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,16 @@
 module UsersHelper
   include SessionsHelper
-  
+
   def active?
     return true unless current_user.active.nil? || !current_user.active
     return false
+  end
+
+  def hasActiveUser?
+    if User.all.where(active: true).size > 0
+      true
+    else
+      false
+    end
   end
 end
