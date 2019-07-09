469c9fb5279f8edccb3d7f7f6e268b29ede04668
Kristi Bresciano <kbresciano@instructure.com>
2019-07-09 12:53:41 -0600

    clock start/stop functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,15 +21,23 @@ class UsersController < ApplicationController
   def start
     @user = User.find_by(id: params[:id])
     @user.update_attribute(:active, true)
+    @user.update_attribute(:clocked_in_time, Time.current)
+    if @user.weekly_time == nil
+      @user.update_attribute(:weekly_time, 0)
+    end
   end
 
   def end
     @user = User.find_by(id: params[:id])
+    @session_time = (Time.current - @user.clocked_in_time).to_i
+
     @user.update_attribute(:active, false)
+    @user.update_attribute(:weekly_time, (@session_time + @user.weekly_time))
+    @user.update_attribute(:clocked_in_time, :nil)
   end
-  
+
   private
-  
+
   def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
