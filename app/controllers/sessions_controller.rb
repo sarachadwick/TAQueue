7a5737b83876eec88f65b219ceeb140239f3f18b
Kristi Bresciano <kbresciano@instructure.com>
2019-07-09 14:42:25 -0600

    keep track of hours on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,14 @@ class SessionsController < ApplicationController
   def destroy
     # If a TA signs out w/o ending their hours, do it for 'em
     @user = User.find_by(id: params[:id])
+    if @user.active
+      @session_time = (Time.current - @user.clocked_in_time).to_i
+
+      @user.update_attribute(:weekly_time, (@session_time + @user.weekly_time))
+      @user.update_attribute(:clocked_in_time, :nil)
+    end
     @user.update_attribute(:active, false)
-    
+
     log_out
     redirect_to root_url
   end
